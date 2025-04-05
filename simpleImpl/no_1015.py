2279ec4cee32ebb47db53e9bb373d765ec90439c
홍일권 <hik10417@gmail.com>
2025-04-05 23:58:28 +0900

    250405

---
+++ simpleImpl/no_1015.py
@@ -0,0 +1,59 @@
+"""
+    수열정렬
+
+
+    수열 P
+
+    배열 A ==> 수열p 적용 ==> 배열 B(비 내림차순)
+
+    B[P[i]] = A[i]
+
+    N은 50보다 작거나 같은 자연수이고, 배열의 원소는 1,000보다 작거나 같은 자연수
+
+    배열 A가 주어졌을떄,
+    비내림차순이 되는 수열을 찾는 프로그램을 작성
+
+    예시)
+    A= [2, 3, 1]  out 수열P 1 2 0
+
+    A[0] = B[P[0]]
+
+    2  = B[P[0]] = B[1]
+    3  = B[P[1]] = B[2]
+    1  = B[P[2]] = B[0]
+
+     A= [2, 3, 1]  >>>> B = [1, 2, 3]
+    B 배열은 A 배열의 비내림차순 배열
+
+    예시)
+    A= [2, 1, 3, 1] out 2 0 3 1
+
+    2  = B[P[0]] = B[2]
+    1  = B[P[1]] = B[0]
+    3  = B[P[2]] = B[3]
+    1  = B[P[3]] = B[1]
+
+    B = [1, 1, 2, 3]
+
+    * P 수열은 A 수열의 각 숫자가 몇번째로 작은지를 0부터 정리한 수열
+"""
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+A = list(map(int, input().split(' ')))
+
+
+if __name__ == '__main__':
+
+    temp = sorted(A)
+
+    p = list()
+    for e in A:
+        p.append(str(temp.index(e)))
+        temp[temp.index(e)] = -1
+
+    print(' '.join(p))
--- simpleImpl/no_7490.py
+++ simpleImpl/no_7490.py
@@ -3,44 +3,58 @@
 
     기본적인 재귀 + 문자열 지식 물어보는 문자
     *문자열 수식 평가 함수 eval()
+
+
+    1. 1에서 n까지 사이에 연산자 3종류를 채운다.
+    2. n-1개를 중복해서 뽑아 채울수 있다.
+    3. 재귀로 구혆
+
+    1 2 3
+     + -
+    1+2-3
+
+
+    00:00
 """
 
-import sys
+# import sys
+#
+# input = sys.stdin.readline
+#
+# tc = int(input())
+# n = 0
+# arr = []
+#
+# answer = []
+#
+# ret = []
+# def go(cnt):
+#     if cnt == 0:
+#
+#         s = arr[0]
+#         for i in range(0, n-1):
+#             s += ret[i]
+#             s += arr[i+1]
+#
+#         if eval(s.replace(' ','')) == 0:
+#             answer.append(s)
+#         return
+#
+#     for item in ['+', '-', ' ',]:
+#         ret.append(item)
+#         go(cnt - 1)
+#         ret.pop()
+#
+#
+# if __name__ == '__main__':
+#     for _ in range(tc):
+#         n = int(input())
+#         arr = [str(i) for i in range(1, n + 1)]
+#         answer = []
+#
+#         go(n - 1)
+#         answer.sort()
+#         for e in answer:
+#             print(e)
+#         print()
 
-input = sys.stdin.readline
-
-tc = int(input())
-n = 0
-arr = []
-
-answer = []
-ret = []
-def go(cnt):
-    if cnt == 0:
-
-        s = arr[0]
-        for i in range(0, n-1):
-            s += ret[i]
-            s += arr[i+1]
-
-        if eval(s.replace(' ','')) == 0:
-            answer.append(s)
-        return
-
-    for item in ['+', '-', ' ',]:
-        ret.append(item)
-        go(cnt - 1)
-        ret.pop()
-
-
-if __name__ == '__main__':
-    for _ in range(tc):
-        n = int(input())
-        arr = [str(i) for i in range(1, n + 1)]
-        answer = []
-
-        go(n - 1)
-        answer.sort()
-        for e in answer:
-            print(e)
-        print()
